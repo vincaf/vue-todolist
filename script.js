dfb08dfb0342100579945448760e113cf55f6f80
vincaf <vincenzocafassog@gmail.com>
2022-07-06 13:17:23 +0200

    an item for each toDo is printed on page - js

---
+++ script.js
@@ -0,0 +1,6 @@
+const app = new Vue({
+    el: "#root",
+    data: {
+        toDos: ['Comprare i pomodori', 'Comprare la pasta', 'Comprare la farina', 'Comprare il sale', 'Comprare la motosega'],
+    },
+})
